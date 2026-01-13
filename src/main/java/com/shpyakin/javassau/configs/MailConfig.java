e09b6135b21c9f5ff5f502ab21fb63df0ff6732e
YourOwnSun <andreyshpyakin1410@yandex.ru>
2026-01-14 01:35:26 +0400

    LR4

--- src/main/java/com/shpyakin/javassau/configs/MailConfig.java
+++ src/main/java/com/shpyakin/javassau/configs/MailConfig.java
@@ -15,8 +15,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("testemail8940@gmail.com");
-        mailSender.setPassword("loremipsum7783");
+        mailSender.setUsername("testemail4534534534@gmail.com");
+        mailSender.setPassword("secret");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
